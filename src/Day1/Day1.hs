af6be0d47055ecab2e820bc35b1dc1cd7fe83676
Brian Jing <bjing@users.noreply.github.com>
2021-12-03 01:07:43 +1100

    Add Day1 solution

---
+++ src/Day1/Day1.hs
@@ -0,0 +1,12 @@
+module Day1 where
+
+main :: IO ()
+main = do
+  pairs <- loadAdjacentPairs
+  print $ length $ filter (\(x, y) -> x < y) pairs
+
+loadAdjacentPairs :: IO [(Int, Int)]
+loadAdjacentPairs = do
+  content <- readFile "src/Day1/input"
+  let readings = (read <$> lines content) :: [Int]
+  return $ zip (init readings) (tail readings)
